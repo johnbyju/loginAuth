0319d8739ab48d05c1ccd1d587d1986ea82d6d27
Johnbyju <138657639+johnbyju@users.noreply.github.com>
2024-05-31 19:20:28 +0530

    Delete config directory

--- config/Db.js
+++
@@ -1,9 +0,0 @@
-import mongoose  from "mongoose";
-
-function dbConnection(){
-    mongoose.connect(process.env.db)
-    .then(res => console.log("Database Connected"))
-    .catch(err => console.log("Databse it's not connected"))
-}
-
-export default dbConnection
